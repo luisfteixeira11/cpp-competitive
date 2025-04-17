8a6ef44bc9a76ea15ab3aecbe005d69a1fbdda57
luis felipe <luisfteixeira117@gmail.com>
2025-04-17 16:57:13 -0300

    problema do codeforces

--- arquivo.cpp
+++ arquivo.cpp
@@ -1,8 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define vi vector<int> //vetor inteiro vai ser "vi"
+#define pb push_back //push back agora vai ser "pb"
 
 int main (){
     int a=1; int b=2;
     int c; cin>>c;
     cout<<a+b+c<<endl;
+    vi v;
+    v.pb(a);
+    for (int i=0; i<1; i++){
+        cout<< v [i] <<endl;
+    }
 }
---
+++ code_forces/deadpixel.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; 
+    cin >> n; 
+    for (int i=0; i<n;i++) {
+        int v1, v2, x, y;
+        cin>>v1>>v2>>x>>y;
+        int areacima = x*v2;             
+        int areabaixo = ((v1-x)-1)*v2;     
+        int areaesq = v1*y;       
+        int areadir = v1*((v2-y)-1);   
+        int maiorarea = max({areacima, areabaixo, areaesq, areadir});
+        cout<<maiorarea<<endl;
+    }
+    return 0;
+}
